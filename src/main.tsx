c22dd4f271abed8af79abeb4dc156441e3ccc899
crow-fox <surakamiya198@gmail.com>
2026-01-24 15:46:05 +0900

    reset css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "remix/component";
 import type { RemixComponent } from "./util";
+import "./global.css";
 
 const App: RemixComponent = () => {
 	return () => <div>Welcome to copagress!</div>;
